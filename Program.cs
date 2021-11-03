7ae94741cb49dbaa50dbd75ccad7efbe65e42324
Wallace Tácio <wallacett.dev@gmail.com>
2021-11-03 09:27:33 -0300

    Adiciona resolução ao exercício Sinal ETEC 2021

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num, den, result;
+
+            Console.Write("Digite o numerador...: ");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite o denominador.: ");
+            den = Convert.ToDouble(Console.ReadLine());
+
+            if(den == 0){
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Não é possível dividir por zero.");
+                Console.ResetColor();
+            }
+            else
+            {
+                result = num / den;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"{num} dividido por {den} é igual a: {result:N2}");
+                Console.ResetColor();
+            }
+            }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Divisao
-Exercício Divisão ETEC 2021
+## Exercício `Divisao` ETEC 2021
+
+Receba um numerador e um denominador. Exiba o resultado da divisão ou "Não é possível dividir por zero.", conforme o caso.
+
+Ex.:
+```
+Digite o numerador....: 12
+Digite o denominador..: 3
+12 dividido por 3 é 4.
+```
+ou
+```
+Digite o numerador....: 0
+Digite o denominador..: 5
+0 dividido por 5 é 0.
+```
+ou
+```
+Digite o numerador....: 12
+Digite o denominador..: 0
+Não é possível dividir por zero.
+```
